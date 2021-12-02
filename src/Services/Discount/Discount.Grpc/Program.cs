83cd12318e9848370c171e4935fafad41d91c125
dwivedi-ankita <ankita.dwivedi@neosoftmail.com>
2021-12-02 18:42:48 +0530

    Grpc Services Added For Basket Microservice

---
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -0,0 +1,28 @@
+using Discount.Grpc.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            host.MigrateDatabase<Program>();
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
